62e31d803ddfd171fee84e0fd2429467b3c1bb2e
haoziapple <haozixiaowang@163.com>
2017-08-18 11:39:20 +0800

    update provider, consumer, zipkinserver to Dalston.SR2
    Signed-off-by: haoziapple <haozixiaowang@163.com>

--- provider/src/main/java/com/haozi/springcloud/provider/controller/ComputeController.java
+++ provider/src/main/java/com/haozi/springcloud/provider/controller/ComputeController.java
@@ -5,24 +5,33 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ComputeController
-{
-	private final Logger logger = LoggerFactory.getLogger(getClass());
-	@Autowired
-	private DiscoveryClient client;
+import java.util.List;
 
-	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public Integer add(@RequestParam Integer a, @RequestParam Integer b)
-	{
-		ServiceInstance instance = client.getLocalServiceInstance();
-		Integer r = a + b;
-		logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
-		return r;
-	}
+@RestController
+public class ComputeController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+
+    @Autowired
+    private Registration registration;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer r = a + b;
+        logger.info("ServiceId: {}", registration.getServiceId());
+        List<ServiceInstance> list = client.getInstances(registration.getServiceId());
+        for (ServiceInstance instance : list) {
+            logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+            logger.info("uri: {}, metaData: {}", instance.getUri(), instance.getMetadata());
+        }
+
+        return r;
+    }
 }
