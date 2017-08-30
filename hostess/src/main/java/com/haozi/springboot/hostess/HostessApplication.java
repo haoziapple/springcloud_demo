c70999185b1409bdb4866a596dbcf409cd4fa0da
haoziapple <haozixiaowang@163.com>
2017-08-30 18:46:52 +0800

    update

--- hostess/src/main/java/com/haozi/springboot/hostess/HostessApplication.java
+++ hostess/src/main/java/com/haozi/springboot/hostess/HostessApplication.java
@@ -5,30 +5,44 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * @className: com.haozi.springboot.hostess.HostessApplication
  * @description: hostess gateway 启动类
  * @author: wanghao/haozixiaowang@163.com
  * @date: 2017/8/30 16:45
-**/
+ **/
 @EnableZuulProxy
 @SpringCloudApplication
 public class HostessApplication {
-    
+
     private static Logger logger = LoggerFactory.getLogger(HostessApplication.class);
+
     /**
      * @Description: 启动main方法
-     * @param: 
+     * @param:
      * @author: wanghao/haozixiaowang@163.com
      * @date: 2017/8/30 16:45
-    **/
+     **/
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         // run code
-        new SpringApplicationBuilder(HostessApplication.class).web(true).run(args);
+        ApplicationContext ctx = new SpringApplicationBuilder(HostessApplication.class).web(true).run(args);
         long end = System.currentTimeMillis();
         logger.info("hostess server start spent time: {} ms", end - start);
 
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            if (beanName.contains("zuul") || beanName.contains("Zuul")) {
+                logger.info("loaded bean's name: {}, class: {}", beanName, ctx.getBean(beanName).getClass().getName());
+            }
+        }
+        ZuulProperties prop = ctx.getBean(ZuulProperties.class);
+        logger.info(prop.getRoutes().toString());
     }
 }
--- hostess/src/main/java/com/haozi/springboot/hostess/config/RoutesLoader.java
+++ hostess/src/main/java/com/haozi/springboot/hostess/config/RoutesLoader.java
@@ -1,7 +1,15 @@
 package com.haozi.springboot.hostess.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Administrator on 2017/8/30.
  */
+@Component
 public class RoutesLoader {
+    @Autowired
+    private Environment env;
+
 }
