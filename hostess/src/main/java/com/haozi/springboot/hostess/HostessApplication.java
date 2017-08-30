5ceed7322c0468f9e0a9eed2ba97fedb91071bca
haoziapple <haozixiaowang@163.com>
2017-08-30 17:02:13 +0800

    update:add logger 使用IDEA的Live Template功能
    Signed-off-by: haoziapple <haozixiaowang@163.com>

--- hostess/src/main/java/com/haozi/springboot/hostess/HostessApplication.java
+++ hostess/src/main/java/com/haozi/springboot/hostess/HostessApplication.java
@@ -1,5 +1,7 @@
 package com.haozi.springboot.hostess;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -13,6 +15,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringCloudApplication
 public class HostessApplication {
+    
+    private static Logger logger = LoggerFactory.getLogger(HostessApplication.class);
     /**
      * @Description: 启动main方法
      * @param: 
@@ -20,6 +24,11 @@ public class HostessApplication {
      * @date: 2017/8/30 16:45
     **/
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        // run code
         new SpringApplicationBuilder(HostessApplication.class).web(true).run(args);
+        long end = System.currentTimeMillis();
+        logger.info("hostess server start spent time: {} ms", end - start);
+
     }
 }
